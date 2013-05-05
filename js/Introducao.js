e5d696be2a7b7996f68bc8f30b4878b93b29c80e
xicoed@gmail.com <xicoed@gmail.com@513996a1-cf44-8dc7-9f25-9942deb90e48>
2013-05-05 05:55:28 +0000

    organizando o código

--- js/Introducao.js
+++ js/Introducao.js
@@ -2,19 +2,19 @@ function Introducao()
 {
 	this.background = new fundo("img/fundo.png", 595, 841); 
 	this.botao_start = new Button("img/Start.png", 339, 41, 240, 390);
-        this.botao_creditos = new Button("img/Creditos.png", 339, 41, 240, 440);
-        this.botao_instrucao = new Button("img/Instrucao.png", 339, 41, 240, 490)
+    this.botao_creditos = new Button("img/Creditos.png", 339, 41, 240, 440);
+    this.botao_instrucao = new Button("img/Instrucao.png", 339, 41, 240, 490)
    
-   this.update=function()
-   {
+	this.update=function()
+	{
    		
    	    this.botao_start.update(); 		
    		this.botao_creditos.update(); 
-  	     this.botao_instrucao.update();
-   };
+  	    this.botao_instrucao.update();
+	};
    
-   this.draw=function()
-   {
+	this.draw=function()
+	{
    		screen.font = "20px Comic Sans MS";
         screen.fillStyle="#000000";
         screen.fillText("Introducao", 20, 20);
@@ -23,48 +23,38 @@ function Introducao()
 		this.botao_start.draw(); 		
    		this.botao_creditos.draw(); 
         this.botao_instrucao.draw();
-
-
-   };
+	};
    
-   this.mouse_down=function(mouse)
-   {
- 
-	if(this.botao_start.clicked(mouse))
-   	{
-   	 currentScene = SCENES.LEVEL_1;	
-   	}
-   	
-   	 if(this.botao_creditos.clicked(mouse))
-   	{
-   	 currentScene = SCENES.CREDITOS;	
-   	}
-   	
-   	if(this.botao_instrucao.clicked(mouse))
-   	{
-   	 currentScene = SCENES.INSTRUCAO;	
-   	}
-   	
-   };
+	this.mouse_down=function(mouse)
+	{
+		if(this.botao_start.clicked(mouse))
+		{
+			currentScene = SCENES.LEVEL_1;	
+		}
+		
+		 if(this.botao_creditos.clicked(mouse))
+		{
+			currentScene = SCENES.CREDITOS;	
+		}
+		
+		if(this.botao_instrucao.clicked(mouse))
+		{
+			currentScene = SCENES.INSTRUCAO;	
+		}
+	};
    
    this.key_down=function(key)
    {
-   	console.log("INTROi keyCode " + key.keyCode );
-   	
-   	
+		console.log("INTROi keyCode " + key.keyCode );
    };
    
    this.mouse_up=function(mouse)
    {
-   	
-   	console.log("INTRO mouse UP X " + mouse.x + " mouse Y " + mouse.y );
-   	
+		console.log("INTRO mouse UP X " + mouse.x + " mouse Y " + mouse.y );
    };
    
    this.key_up=function(key)
    {
-   	console.log("INTRO key UP keyCode " + key.keyCode );
+		console.log("INTRO key UP keyCode " + key.keyCode );
    };
-
-   
 }
--- js/LEVEL_1.js
+++ js/LEVEL_1.js
@@ -3,45 +3,39 @@ function Level_1()
 	this.nuvem = new Nuvem("img/nuvem.png", 124, 76,
 	 (SCREENWIDTH-210)/2, 0);
 	 
-	 this.AsteroidManager = new Asteroid("img/asteroide_grande1.png", 220, 220);
+	this.AsteroidManager = new Asteroid("img/asteroide_grande1.png", 220, 220);
 
-   this.update=function()
-   {
- 		this.nuvem.update();
- 		
+	this.update=function()
+	{
+		this.nuvem.update();
  		this.AsteroidManager.update();	
- 		
-   };
+	};
    
-   this.draw=function()
-   {    	
+	this.draw=function()
+	{    	
    		this.nuvem.draw();
-   		
  		this.AsteroidManager.draw();	
- 					
-
-   };
+	};
        
-   this.mouse_down=function(mouse)
-   {
+	this.mouse_down=function(mouse)
+	{
    		console.log("LEVEL_1  mouse X " + mouse.x + " mouse Y " + mouse.y );
-   };
+	};
 
-   this.mouse_up=function(mouse)
-   {
+	this.mouse_up=function(mouse)
+	{
    	
-   	
-   };
+	};
 
-   this.key_down=function(key)
-   {
+	this.key_down=function(key)
+	{
    		this.nuvem.key_down(key);
    		//this.AsteroidManager.key_down(key);
-   };
+	};
    
-   this.key_up=function(key)
-   {
+	this.key_up=function(key)
+	{
    		this.nuvem.key_up(key);
    		//this.AsteroidManager.key_up(key);
-   };    
+	};    
 }
